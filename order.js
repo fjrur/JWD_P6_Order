6a24038697f8ced4948902a4b5a0996e7b8389c3
fjrur <137527697+fjrur@users.noreply.github.com>
2023-07-09 12:04:16 +0700

    Add files via upload

---
+++ order.js
@@ -0,0 +1,49 @@
+let itemOrder = [
+    {
+        dipesan : false,
+        harga : 15000,
+        nama : 'Bakso',
+    },
+    {
+        dipesan : false,
+        harga : 25000,
+        nama : 'Nasgor',
+    },
+    {
+        dipesan : false,
+        harga : 10000,
+        nama : 'Wedang Jahe',
+    },
+    {
+        dipesan : false,
+        harga : 5000,
+        nama : 'Es Teh',
+    }
+]
+
+let totalOrder = 0;
+
+const itemTerpilih = document.getElementsByClassName('item');
+
+const textTotal = document.getElementById('last-item');
+
+for(let i = 0; i < itemTerpilih.length; i++){
+    itemTerpilih[i].addEventListener('click', function(){
+        setItemPemesanan(i, itemTerpilih[i]);
+    });
+}
+
+function setItemPemesanan(index, elemen){
+    if (itemOrder[index].dipesan == false){
+        itemOrder[index].dipesan = true;
+        elemen.style.backgroundColor = "darkcyan";
+        elemen.style.color = "white";
+        totalOrder = totalOrder + itemOrder[index].harga;
+    } else{
+        itemOrder[index].dipesan = false;
+        elemen.style.backgroundColor = "aqua";
+        elemen.style.color = "black";
+        totalOrder = totalOrder - itemOrder[index].harga;
+    }
+    textTotal.innerHTML = "Total <span>Rp. " + totalOrder + "</span>"
+}
